16c1b49a0d5e78c2cdf696b375bb79a88d7ce077
Deerain <1deerain1@gmail.com>
2026-03-18 02:18:05 +0300

    refactoring

--- ubm-dots.py
+++ ubm-dots.py
@@ -6,133 +6,222 @@ import json
 import subprocess
 import os
 import pathlib
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Any
 from enum import Enum
 
 REPO_OWNER = "deeerain"
 REPO_NAME = "ubm-dots"
-REPO_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/releases/latest"
 INSTALL_FOLDER = pathlib.Path("/usr/share/ubm-dots")
 DOTFILES_DIR_NAME = "dots"
 
 app = typer.Typer()
 
 
-class Service(Enum):
-    HYPRLAND = "hyprland"
-    WAYBAR = "waybar"
+class ServiceBase:
+    def stop(self):
+        raise NotImplementedError()
+
+    def start(self):
+        raise NotImplementedError()
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def _cmd(self, cmd):
+        try:
+            subprocess.run(cmd)
+        except subprocess.CalledProcessError as e:
+            pass
 
 
-def get_latest_repo_info(owner: str, repo_name):
-    url = f"https://api.github.com/repos/{owner}/{repo_name}/releases/latest"
+class Utils:
+    @staticmethod
+    def get_current_version(repo_name: str):
+        '''Get installed verison'''
+        result = subprocess.run(
+            f"pacman -Q {REPO_NAME}".split(), capture_output=True)
+        version = result.stdout.decode("utf-8").split()[1]
 
-    request = urllib.request.Request(url)
+        if version.startswith("v"):
+            version = version[1:]
 
-    with urllib.request.urlopen(request) as response:
-        data = json.loads(response.read().decode("utf-8"))
-        return data
+        return version
 
+    @staticmethod
+    def is_newest_version(ver1: str, ver2: str) -> bool:
+        ver1_parts = list(map(int, ver1.replace("-", ".").split(".")))
+        ver2_parts = list(map(int, ver2.replace("-", ".").split(".")))
 
-def setup_zst(filepath: pathlib.Path):
-    if not filepath.exists():
-        return
+        while len(ver1_parts) < 3:
+            ver1_parts.append(0)
+        while len(ver2_parts) < 3:
+            ver2_parts.append(0)
 
-    subprocess.run(f"sudo pacman -U {filepath}".split())
+        for i in range(3):
+            if ver1_parts[i] > ver2_parts[i]:
+                return True
+            elif ver1_parts[i] < ver2_parts[i]:
+                return False
 
+        return False
 
-def download_zst(download_url: str, filepath: pathlib.Path) -> Optional[str]:
-    result = urllib.request.urlretrieve(download_url, filepath)
-    if len(result) > 0:
-        return result[1]
-    return None
-
-
-def find_zst_file(assets: List[Dict]) -> Optional[Dict]:
-    for asset in assets:
-        filename = asset.get("name")
-
-        if not filename:
-            continue
-
-        if filename.endswith(".zst"):
-            return asset
-
-
-def get_current_version() -> str:
-    result = subprocess.run(f"pacman -Q {REPO_NAME}".split(), capture_output=True)
-    version = result.stdout.decode("utf-8").split()[1]
-
-    if version.startswith("v"):
-        version = version[1:]
-
-    return version
+    @staticmethod
+    def setup_zst(filepath: pathlib.Path):
+        subprocess.run(f"sudo pacman -U {filepath}".split())
 
 
-def is_newest_version(ver1: str, ver2: str) -> bool:
-    ver1_parts = list(map(int, ver1.replace("-", ".").split(".")))
-    ver2_parts = list(map(int, ver2.replace("-", ".").split(".")))
+class UpdateService:
+    def __init__(self, repo_name: str, repo_owner: str) -> None:
+        self.repo = repo_name
+        self.owner = repo_owner
+        self.assets: List[Dict] = {}
+        self.latest_version: str = None
 
-    while len(ver1_parts) < 3:
-        ver1_parts.append(0)
-    while len(ver2_parts) < 3:
-        ver2_parts.append(0)
+    @property
+    def api_url(self) -> str:
+        return f'https://api.github.com/repos/{self.owner}/{self.repo}'
 
-    for i in range(3):
-        if ver1_parts[i] > ver2_parts[i]:
-            return True
-        elif ver1_parts[i] < ver2_parts[i]:
-            return False
+    def check_updates(self) -> True:
+        pass
 
-    return False
+    def get_latest_repo_info(self) -> None:
+        data = self._request(f'{self.api_url}/releases/latest')
+
+        self.assets = data.get('assets')
+
+        latest_version = data.get('tag_name')
+
+        if latest_version.startswith("v"):
+            latest_version = latest_version[1:]
+
+            self.latest_version = latest_version
+
+    def find_zst_file(self) -> Dict | None:
+        for asset in self.assets:
+            filename = asset.get("name")
+
+            if not filename:
+                continue
+
+            if filename.endswith(".zst"):
+                return asset
+
+    def download_asset(self, asset: Dict, folder: str = '/tmp/') -> str | None:
+        name, url = self._get_asset_download_info(asset)
+        download_path = pathlib.Path(folder, name)
+        return self._download(url, download_path)
+
+    def _download(self, url: str, filename: str) -> str | None:
+        result = urllib.request.urlretrieve(url, filename)
+
+        if len(result) > 0:
+            return result[1]
+        return None
+
+    def _get_asset_download_info(self, asset: Dict) -> (str, str):
+        return (asset.get('name'), asset.get('browser_download_url'))
+
+    def _request(self, url: str, *, method: str = 'get') -> Any:
+        req = urllib.request.Request(url, method=method)
+
+        with urllib.request.urlopen(req) as resp:
+            return Dict(json.loads(resp.read().decode('utf-8')))
+
+
+class HyprlandService(ServiceBase):
+    def restart(self):
+        self._cmd(['hyprctl', 'reload'])
+
+
+class WaybarService(ServiceBase):
+    def stop(self):
+        self._cmd(['killall', 'waybar'])
+
+    def start(self):
+        self._cmd(['hyprctl', 'dispatch', 'exec', 'waybar'])
+
+
+class UBMService(ServiceBase):
+    def __init__(self, services: Dict[str, ServiceBase] = {}):
+        self.services = services
+
+    def add_service(self, name: str, service: ServiceBase):
+        self.services.setdefault(name, service)
+
+    def get_service(self, name: str) -> ServiceBase | None:
+        return self.services.get(name)
+
+    def restart(self):
+        for service in self.services.values():
+            service.restart()
+
+    def start(self):
+        for service in self.services.values():
+            try:
+                service.restart()
+            except NotImplementedError:
+                pass
+
+    def stop(self):
+        for service in self.services.values():
+            try:
+                service.stop()
+            except NotImplementedError:
+                pass
+
+
+class UBM:
+    def __init__(self, update_service: UpdateService):
+        self.update_service = update_service
+        self.services = UBMService()
+
+    def add_service(self, name: str, service: ServiceBase):
+        self.services.add_service(name, service)
+
+    def restart_services(self, services: List[str] = []):
+        if not any(services):
+            self.services.restart()
+            return
+
+        for service_name in services:
+            self.services.get_service(service_name).restart()
+
+    def get_service_names(self) -> List[str]:
+        return self.services.services.keys()
+
+    def update(self):
+        zst_file = update_service.download_asset()
+        Utils.setup_zst(zst_file)
+
+    def check_updates(self) -> bool:
+        return self.update_service.check_updates()
+
+
+update_service = UpdateService(REPO_NAME, REPO_OWNER)
+ubm = UBM(update_service)
+ubm.add_service('hyprland', HyprlandService())
+ubm.add_service('waybar', WaybarService())
+
+
+# COMMANDS
 
 
 @app.command("update")
 def update():
-    repo_info = get_latest_repo_info(REPO_OWNER, REPO_NAME)
-    assets = repo_info.get("assets")
-    asset = find_zst_file(assets)
-
-    filename = asset.get("name")
-    filepath = pathlib.Path("/tmp/", filename)
-    download_url = asset.get("browser_download_url")
-
-    latest_version = repo_info.get("tag_name")
-    current_version = get_current_version()
-
-    if latest_version.startswith("v"):
-        latest_version = latest_version[1:]
-
-    if not is_newest_version(latest_version, current_version):
+    if not ubm.check_updates():
+        typer.echo('Latest version')
         return
 
-    download_zst(download_url, filepath)
-
-    setup_zst(filepath)
-
-    if typer.confirm("Clean cache?", default=True):
-        typer.echo(f"   Remove {filepath}")
-        if filepath.exists():
-            os.remove(filepath)
+    ubm.update()
 
 
 @app.command("reload")
 def reload(
-    services: List[Service] = typer.Option(
-        [Service.HYPRLAND, Service.WAYBAR], "--services", "-s"
-    ),
+    services: List[str] = typer.Option(
+        [*ubm.get_service_names()], "--services", "-s"),
 ):
-    for service in services:
-        try:
-            match service:
-                case Service.HYPRLAND:
-                    typer.echo("Reloading hyprland...", color=True)
-                    subprocess.run(["hyprctl", "reload"])
-                case Service.WAYBAR:
-                    typer.echo("Reloading waybar")
-                    subprocess.run(["killall", "waybar"])
-                    subprocess.run(["hyprctl", "dispatch", "exec", "waybar"])
-        except subprocess.CalledProcessError as e:
-            typer.echo(f"Called error: {e}")
+    ubm.restart_services(services)
 
 
 def backup(*args: pathlib.Path):
